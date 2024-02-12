7cd8754c32f057c9e144f8569bf5569c94ada1a0
Archit Das <archit.das@berkeley.edu>
2024-02-12 00:20:30 -0800

    added comments to rgb mixer

--- project 1 - expressive light/rgb_mixer.ino
+++ project 1 - expressive light/rgb_mixer.ino
@@ -7,6 +7,8 @@ int red_button_pin = 2;
 int green_button_pin = 3;
 int blue_button_pin = 4;
 
+// old values used to store led value of previous iteration
+// they are used for toggle button implementation
 int red_button_old = 0;
 int green_button_old = 0;
 int blue_button_old = 0;
@@ -30,24 +32,18 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  analogWrite(red_LED_pin, 127);
-//  analogWrite(blue_LED_pin, 127);
-//  analogWrite(green_LED_pin, 127);
-
+    // reading button values for current iteration
      int red_button_new = digitalRead(red_button_pin);
      int green_button_new = digitalRead(green_button_pin);
      int blue_button_new = digitalRead(blue_button_pin);
 
-     // todo: use a for loop for this, or use a function
-
-     if (red_button_old == 0 && red_button_new == 1) {
-      if (red_state == 0) { 
+     if (red_button_old == 0 && red_button_new == 1) { // checking if button value has changed this iteration compared to previous
+      if (red_state == 0) { // toggle to on if off previously
         analogWrite(red_LED_pin, 0);
         red_state = 1;
       }
       else {
-        analogWrite(red_LED_pin, 127);
+        analogWrite(red_LED_pin, 127); // toggle off if one previously
         red_state = 0;
       }
      }
@@ -74,9 +70,10 @@ void loop() {
       }
      }
      
+     // store this current iteration's values for next iteration
      red_button_old = red_button_new;
      green_button_old = green_button_new;
      blue_button_old = blue_button_new;
      
-   delay(100);
+   delay(100); // delay added for polling
 }
